3d3ff265ac3f128d3dc6b41838ee998fab2294a7
Наиль Усманов <nailusmanov@MacBook-Pro-Nail.local>
2024-12-16 23:07:55 +0300

    second commit

--- cache/cache.go
+++ cache/cache.go
@@ -1,35 +1,45 @@
 package cache
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 type Cache struct {
-    val map[string]interface{}
+	val map[string]interface{}
 }
 
 func New() *Cache {
-    return &Cache{
-        make(map[string]interface{}),
-    }
+	return &Cache{
+		make(map[string]interface{}),
+	}
 }
 
-func (c *Cache) Set(key string, value interface{}) error {
-    if key != "" {
-        c.val[key] = value
-        return nil
-    }
-    return errors.New("empty key")
+func (c *Cache) Set(key string, value interface{}, t int) error {
+	if key != "" {
+		go func() {
+
+			timer := time.NewTimer(time.Duration(t) * time.Second)
+			<-timer.C
+			delete(c.val, key)
+		}()
+		c.val[key] = value
+		return nil
+	}
+	return errors.New("empty key")
+
 }
 
 func (c *Cache) Get(key string) (interface{}, bool) {
-    value, exists := c.val[key]
-    return value, exists
+	value, exists := c.val[key]
+	return value, exists
 }
 
 func (c *Cache) Delete(key string) error {
-    _, exists := c.val[key]
-    if exists {
-        delete(c.val, key)
-        return nil
-    }
-    return errors.New("invalid key")
+	_, exists := c.val[key]
+	if exists {
+		delete(c.val, key)
+		return nil
+	}
+	return errors.New("invalid key")
 }
--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 func main() {
 	newCache := cache.New()
 
-	newCache.Set("userId1", 123)
-	newCache.Set("userId2", "456")
-	newCache.Set("userId3", "789")
+	newCache.Set("userId1", "123", 5)
+	newCache.Set("userId2", "456", 5)
+	newCache.Set("userId3", "789", 5)
 
 	value, exists := newCache.Get("userId3")
 	if exists {
@@ -21,10 +21,18 @@ func main() {
 		fmt.Println("Key userId3 not found")
 	}
 
+	value, exists = newCache.Get("userId2")
+	if exists {
+		fmt.Println("Value for userId2:", value)
+	} else {
+		fmt.Println("Key userId2 not found")
+	}
+
 	err := newCache.Delete("userId1")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	fmt.Println("Cache state:", newCache)
+
 }
